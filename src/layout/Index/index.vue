5b41a61ce2062eb29173e92d0428c59689082787
李庆松 <lqs@ecrrc.com>
2020-01-20 17:49:08 +0800

    IndexLayout 布局

--- src/layout/Index/index.vue
+++ src/layout/Index/index.vue
@@ -1,5 +1,15 @@
 <template>
-    <app-main />
+  <div id="indexlayout">
+    <div id="indexlayout-left">
+      left
+    </div>
+    <div id="indexlayout-right">
+        <div id="indexlayout-right-top">
+          top
+        </div>
+        <app-main />
+    </div>    
+  </div>
 </template>
 <script>
 /** 
@@ -14,3 +24,26 @@ export default {
   }
 };
 </script>
+<style lang="scss" scoped>
+#indexlayout {
+  display: flex;
+  height: 100vh;
+  overflow: hidden;
+  background-color: aqua;
+}
+#indexlayout-left {
+  width: 200px;
+  height: 100vh;
+  background-color: aquamarine;
+}
+#indexlayout-right {
+  flex: 1;
+  overflow: auto;
+  background-color: bisque;
+}
+#indexlayout-right-top {
+   width: 100%;
+   height: 60px;
+   background-color: cadetblue;
+}
+</style>
