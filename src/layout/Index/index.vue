0ac8eba55a0109ca76017080dd471a418c468063
lqs <957698457@qq.com>
2020-02-21 18:29:39 +0800

    IndexLayout 布局

--- src/layout/Index/index.vue
+++ src/layout/Index/index.vue
@@ -7,28 +7,43 @@
         </router-link>
       </div>
       <div class="indexlayout-left-menu">
-        <el-scrollbar wrap-class="scrollbar-wrapper">
-        左侧导航<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        123<br>
-        456<br>
-        </el-scrollbar>
+          <el-scrollbar class="flex-scrollbar" wrap-class="default-scrollbar__wrap">
+              <el-menu
+                default-active="2"
+                class="el-menu-vertical-demo">
+                <el-submenu index="1">
+                  <template slot="title">
+                    <i class="el-icon-location"></i>
+                    <span>导航一</span>
+                  </template>
+                  <el-menu-item-group>
+                    <template slot="title">分组一</template>
+                    <el-menu-item index="1-1">选项1</el-menu-item>
+                    <el-menu-item index="1-2">选项2</el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group title="分组2">
+                    <el-menu-item index="1-3">选项3</el-menu-item>
+                  </el-menu-item-group>
+                  <el-submenu index="1-4">
+                    <template slot="title">选项4</template>
+                    <el-menu-item index="1-4-1">选项1</el-menu-item>
+                  </el-submenu>
+                </el-submenu>
+                <el-menu-item index="2">
+                  <i class="el-icon-menu"></i>
+                  <span slot="title">导航二</span>
+                </el-menu-item>
+                <el-menu-item index="3" disabled>
+                  <i class="el-icon-document"></i>
+                  <span slot="title">导航三</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                  <i class="el-icon-setting"></i>
+                  <span slot="title">导航四</span>
+                </el-menu-item>
+              </el-menu>
+        
+          </el-scrollbar>
       </div>
     </div>
     <div id="indexlayout-right" class="fiexd-header">
@@ -38,7 +53,13 @@
                  <svg-icon icon-class="s-fold" />
                </div>
                <div class="indexlayout-top-menu">
-                顶部导航              
+                  <el-scrollbar class="horizontal-scrollbar">
+                        <a class="indexlayout-top-menu-li active" href="javascript:void(null);">首页</a> 
+                        <a class="indexlayout-top-menu-li" href="javascript:void(null);">图表统计</a>
+                        <a class="indexlayout-top-menu-li" href="javascript:void(null);">列表页</a>
+                        <a class="indexlayout-top-menu-li" href="javascript:void(null);">发布页</a> 
+                        <a class="indexlayout-top-menu-li" href="javascript:void(null);">详情页</a> 
+                  </el-scrollbar>
                </div>
                <div class="indexlayout-top-menu-right">
                  个人信息
@@ -88,7 +109,7 @@ export default {
   width: $leftSideBarWidth;
   height: 100vh;
   background-color: $leftBgColor;
-  color: #fff;
+  color: $topMenuFontColor;
   .indexlayout-left-logo{
     width: 100%;
     height: $headerHeight;
@@ -103,21 +124,24 @@ export default {
       .logo-title {
         display: inline-block;
         margin: 0;
-        font-size: 18px;
+        font-size: 16px;
         font-family: Roboto,sans-serif;
-        color: #fff;
+        color: $topMenuFontColor;
       }
     }
     
   }
   .indexlayout-left-menu{
     flex: 1;
+    display: flex;
     /* overflow: auto; */
-    .el-scrollbar {
-        height: 100vh;
-    }
+    // height: calc(100vh);
+    /* .el-scrollbar {
+        flex:1;
+    } */
   }
 }
+
 #indexlayout-right {
   position: relative;
   flex: 1;
@@ -132,7 +156,7 @@ export default {
       width: 100%;
       height: $headerHeight;
       background-color: $leftBgColor;
-      color: #FFFFFF;
+      color: $topMenuFontColor;
       .indexlayout-flexible,
       .indexlayout-top-menu-right,
       .indexlayout-top-menu{
@@ -142,10 +166,10 @@ export default {
       .indexlayout-flexible{
         width: $headerHeight;
         text-align: center;
-        font-size: 20px;
         cursor: pointer;
         &:hover{
-          background-color: $leftMenuActiveBgColor;
+          background-color: $topMenuHoverBgColor;
+          color: $topMenuHoverFontColor;
         }
       }
       .indexlayout-top-menu-right{
@@ -153,10 +177,33 @@ export default {
       }
       .indexlayout-top-menu{
         flex: 1;
+        display: flex;
         overflow: hidden;
         /* overflow-x: auto; */
+        .indexlayout-top-menu-li{
+          display: inline-block;
+          padding: 0 15px;
+          height: ($headerHeight)-3;
+          text-decoration: none;
+          color: $topMenuFontColor;
+          font-size: 15px;
+          border-bottom: solid 3px $leftBgColor;
+          &:hover,
+          &.active{
+            background-color: $topMenuHoverBgColor;
+            color: $topMenuHoverFontColor;
+            border-bottom-color: $leftMenuActiveBgColor;
+          }
+          &.active{
+            background-color: $topMenuActiveBgColor;
+          }
+        }
         .breadcrumb{
           line-height: $headerHeight;
+          .el-breadcrumb__item{
+            display: inline-block;
+            float: none;
+          }
         }
       }
     }
