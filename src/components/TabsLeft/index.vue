12de751809370d3c9f4cdfbf3a3e2993e20b7587
李庆松 <lqs@ecrrc.com>
2020-03-13 17:19:25 +0800

    自定义组件

---
+++ src/components/TabsLeft/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <el-tabs class="tabsleft" tab-position="left" :style="{height: height}" >
+        <slot></slot>
+    </el-tabs>
+</template>
+<script>
+export default {
+    name: 'TabsLeft',
+    props: {
+        height: {
+            type: String,
+            required: true
+        }
+    },
+    data() {
+        return {
+        };
+    },
+    methods: {
+    },
+    mounted() {
+    }
+};
+</script>
+<style lang="scss" scoped>
+.tabsleft {
+    /deep/ .el-tabs__content {
+        height: 100%;
+        overflow: auto;
+    }
+}
+</style>
