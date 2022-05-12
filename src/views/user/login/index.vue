dedb1103ee94215780afb2d861779f6cb9802e56
lqs <957698457@qq.com>
2022-05-12 18:00:00 +0800

    update UserLayout

--- src/views/user/login/index.vue
+++ src/views/user/login/index.vue
@@ -144,12 +144,14 @@ export default defineComponent({
   margin: 0 auto;
   border-radius: 4px;
   background-color: rgba(255, 255, 255, 0.2);
+  box-shadow: 0 0 10px rgba(0,0,0,.1);
   .title {
     font-size: 28px;
     margin-top: 0;
     margin-bottom: 30px;
     text-align: center;
-    color: #ffffff;
+    // color: #ffffff;
+    color: rgba(0,0,0,.85);
     /* background-image:-webkit-linear-gradient(right,#FFFFFF,#009688, #FFFFFF); 
         -webkit-background-clip: text; 
         -webkit-text-fill-color:transparent; */
